131633589a36059cc21c5bb9f0b98c3b95acaed8
Ilya Krasulia <95487847+IlyaKrasulia@users.noreply.github.com>
2024-03-28 15:22:46 +0200

    Update main.js

--- main.js
+++ main.js
@@ -1,23 +1,34 @@
-var stickyBlock = document.querySelector(".sticky-block");
-var viewOffersBlock = document.querySelector(".view-offers");
-var stickyBlockTop = stickyBlock.offsetTop;
-var lastScrollPosition = 0;
+const stickyBlock = document.querySelector(".sticky-block");
+const viewOffersBlock = document.querySelector(".view-offers");
+const footer = document.querySelector(".footer");
+const stickyBlockTop = stickyBlock.offsetTop;
+const footerTop = footer.offsetTop;
+let lastScrollPosition = 0;
 
 window.addEventListener("scroll", function () {
-  var scrollPosition = window.scrollY;
+  let scrollPosition = window.scrollY;
+
+  console.log(lastScrollPosition, " => lastScrollPosition");
+  console.log(scrollPosition, " => scrollPosition");
 
   if (scrollPosition >= stickyBlockTop) {
     stickyBlock.classList.add("sticky");
-    // Check if scrolling down
-    if (scrollPosition > lastScrollPosition) {
-      viewOffersBlock.style.display = "none";
-    } else {
-      viewOffersBlock.style.display = "block";
-    }
   } else {
     stickyBlock.classList.remove("sticky");
     viewOffersBlock.style.display = "block";
   }
+
+  if (scrollPosition - 150 >= footerTop) {
+    stickyBlock.classList.remove("sticky");
+  }
+
+    // if (stickyBlock.classList.contains("sticky")) {
+    //   if (scrollPosition > lastScrollPosition) {
+    //     viewOffersBlock.style.display = "none";
+    //   } else {
+    //     viewOffersBlock.style.display = "block";
+    //   }
+    // }
 
   lastScrollPosition = scrollPosition;
 });
